7a40c90814b76c26af8be9255d84172d47d79d0c
mflanigan <mflanigantwualumn@gmail.com>
2022-02-23 18:00:05 -0600

    fixed pathways

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const exphbs = require("express-handlebars");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require("./config/connection.js");
+const sequelize = require("./config/config");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
@@ -32,7 +32,7 @@ app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(require('./controllers/'));
